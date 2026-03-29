368435a67617229c1f7aae25ce6d97eab663731d
IDGS802-23002162 <86053@alumnos.utleon.edu.mx>
2026-03-29 16:27:22 -0600

    front 2/3

--- modulos_routes/ecommerce/routes.py
+++ modulos_routes/ecommerce/routes.py
@@ -1,5 +1,5 @@
 
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 from sqlalchemy import extract, func
 from models import db
 
@@ -10,3 +10,17 @@ ecommerce_bp=Blueprint('ecommerce',__name__)
 def ecommerce():
 
     return render_template("modulos_front/ecommerce/ecommerce.html")
+
+@ecommerce_bp.route("/pagar", methods=["GET", "POST"])
+def pagar():
+
+    if request.method == "POST": 
+        return redirect(url_for('ecommerce.ticket'))
+
+    return render_template("modulos_front/ecommerce/pago.html")
+
+
+@ecommerce_bp.route("/ticket", methods=["GET", "POST"])
+def ticket():
+
+    return render_template("modulos_front/ecommerce/ticket.html")
